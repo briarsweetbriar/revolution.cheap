55e0e13b24a403ac3638060a9a14260a775c956e
timothythehuman <timothythehuman@gmail.com>
2014-06-30 14:58:35 -0500

    precompile font assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Precompile additional assets
+  config.assets.precompile += %w( .otf .ttf )
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
