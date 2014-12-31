9ecfbf026965f69f00fd937867d5ecc044dd0772
commoner <timothy.baron@gmail.com>
2014-12-31 10:18:44 -0800

    simplify header and nav

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
+set :password, ask('Server password:', nil)
  
 # Replace 127.0.0.1 with your server's IP address!
-server '104.131.228.131', user: 'deploy', roles: %w{web app}
+server '104.131.228.131', user: 'deploy', roles: %w{web app}, password: fetch(:password)
